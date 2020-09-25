e1c6937be0856941039be4325f9c8c394016f3d0
Esteban Ginez <eginez@gmail.com>
2020-09-25 14:06:10 +0200

    adding a simple install.sh script

---
+++ tf/install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install jq unzip git
+git clone https://github.com/GoogleCloudPlatform/terraform-google-examples.git
+cd terraform-google-examples
+sh terraform-install.sh
+
+echo Run terraform init to finish
+
