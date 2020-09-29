26d940695a1a9e5af2212c0280d4a55902fe025f
Wessel van de Brug <wvdbrug@gmail.com>
2020-09-29 23:03:42 +0200

    Clean up information message

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -12,13 +12,8 @@ def main():
     argument_parser.add_argument("-o", "--output", required=False, help="Path to output CSV file")
     arguments = argument_parser.parse_args()
 
-    message = f"Analysing \"{arguments.input}\" and outputting to "
-    if arguments.output:
-        message += f"\"{arguments.output}\""
-    else:
-        message += "console"
-    message += "."
-    print(message)
+    output = arguments.output if arguments.output else "console"
+    print(f"Analysing \"{arguments.input}\" and outputting to {output}")
 
 
 if __name__ == '__main__':
