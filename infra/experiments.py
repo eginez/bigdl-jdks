270493f24c3a83f104761a9b981a93b53139a9cf
Esteban Ginez <eginez@gmail.com>
2020-10-03 09:31:42 +0200

    adding ml script run

--- infra/experiments.py
+++ infra/experiments.py
@@ -5,6 +5,16 @@ import csv
 import subprocess
 import os
 
+def exec_cmd(cmd, env):
+    if dry-run:
+        cmd_str = ' '.join(cmd)
+        print(f'Will run command: {cmd_str} with env {env}')
+    else:
+        print(f'Executing command: {cmd_str} with env {env}')
+        subprocess.run(cmd, env=env)
+
+
+
 def run_terraform(experiment):
     print(f'Will run experiment with setup: {experiment}')
     run_env = os.environ.copy()
@@ -19,8 +29,8 @@ def run_terraform(experiment):
 
     cores = experiment['cores']
     ## minimum number or cores is 2
-    if cores == 1:
-        cores = 2
+    if int(cores) == 1:
+        cores = "2"
 
     machine_type = f'e2-highmem-{cores}'
     run_env['TF_VAR_machine_type'] = machine_type
@@ -29,8 +39,29 @@ def run_terraform(experiment):
     run_env['TF_VAR_batch_size'] = batch
 
     cmd_str = ' '.join(cmd)
-    print(f'Will run command: {cmd_str} with env {run_env}')
-    subprocess.run(cmd, env=run_env)
+    print(f'will run command: {cmd_str} with env {run_env}')
+
+    exec_cmd(cmd, run_env)
+    #subprocess.run(cmd, env=run_env)
+
+def run_ml(master_ip, local_path_ml_script):
+    remote_ml_dir = '/tmp/ml_scripts'
+    #create a dir in well know location in master
+    cmd_create_dir = ['ssh', master_ip, 'mkdir', '-p', remote_ml_dir]
+
+    exec_cmd(cmd,cmd_create_dir)
+    #subprocess.run(cmd_create_dir)
+
+    #copy ml_script to to 
+    copy_cmd = ['scp',local_path_ml_script, f"{master_ip}:{remote_ml_dir}"]
+    #subprocess.run(copy_cmd)
+    exec_cmd(copy_cmd)
+
+    script_name=os.path.basename(local_path_ml_script)
+    #run script in master
+    run_script = ['ssh', master_ip, "bash", f"{remote_ml_dir}/{script_name}"]
+    #subprocess.run(copy_cmd)
+    exec_cmd(run_script)
 
 
 if __name__ == '__main__':
@@ -38,6 +69,7 @@ if __name__ == '__main__':
     argument_parser = ArgumentParser()
     argument_parser.add_argument("-p", "--plan", required=True, help="path to the plan of experiments CSV output file")
     argument_parser.add_argument("-e", "--experiment", type=int, required=True, help="index of the experiment to run", default=0)
+    argument_parser.add_argument("-m", "--ml", required=True, help="path to the ml script")
 
     arguments = argument_parser.parse_args()
     csv_file = csv.reader(open(arguments.plan))
@@ -51,6 +83,7 @@ if __name__ == '__main__':
                 'batch': l[4]
                 }
         all_exp.append(exp)
+
     if arguments.experiment == -1:
         # run all one-by-one
         print('will run all experiments')
@@ -58,6 +91,8 @@ if __name__ == '__main__':
     else:
         #run a single experiment
         run_terraform(all_exp[arguments.experiment])
+    ## run the ml command
+    run_ml('10.1.1.0', arguments.ml)
 
 
 
