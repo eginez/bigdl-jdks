c85e77a72708a5fd1acd255cca9790e11fb5877b
Esteban Ginez <175813+eginez@users.noreply.github.com>
2020-10-03 13:52:06 +0200

    Update clusterlenet.sh

--- ml/clusterlenet.sh
+++ ml/clusterlenet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 scp lenet5.py  $@:/tmp/ml_scripts
 ## TODO add more setup steps
+## $1 first parameter $2 param, and so on
 
 ssh $@ /home/am72ghiassi/bd/spark/bin/spark-submit --master spark://$@:7077 --driver-cores 1 --driver-memory 1G --total-executor-cores 2 --executor-cores 1 --executor-memory 1G --py-files /home/am72ghiassi/bd/spark/lib/bigdl-0.11.0-python-api.zip,/tmp/ml_scripts/lenet5.py --properties-file /home/am72ghiassi/bd/spark/conf/spark-bigdl.conf --jars /home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.driver.extraClassPath=/home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.executer.extraClassPath=bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar  /tmp/ml_scripts/lenet5.py --action train --dataPath /tmp/mnist
