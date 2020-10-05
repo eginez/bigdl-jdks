a60641452ba142853ac08bdc9cae2986c001e1c5
shreyan2020 <60085842+shreyan2020@users.noreply.github.com>
2020-10-05 13:35:01 +0200

    Update clusterlenet.sh
    fixing bugs

--- ml/clusterlenet.sh
+++ ml/clusterlenet.sh
@@ -8,5 +8,5 @@ scp lenet5.py  $1:/tmp/ml_scripts
 
 ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/spark/bin/spark-submit --master spark://$1:7077 --driver-cores 1 --driver-memory 1G --total-executor-cores 2 --executor-cores 1 --executor-memory 1G --py-files /home/am72ghiassi/bd/spark/lib/bigdl-0.11.0-python-api.zip,/tmp/ml_scripts/lenet5.py --properties-file /home/am72ghiassi/bd/spark/conf/spark-bigdl.conf --jars /home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.driver.extraClassPath=/home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.executer.extraClassPath=bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar  /tmp/ml_scripts/lenet5.py --action train --dataPath /tmp/mnist -b $2
 mkdir -p results
-scp $1:/tmp/ml_scripts/lenet5.py/result.csv results/$3.csv
+scp $1:/tmp/ml_scripts/result.csv results/$3.csv
 gsutil cp results/$3.csv gs://qpecs-output
