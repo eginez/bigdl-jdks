db12207c516b35ac121ebc91eda7dbfd519eb11f
Esteban Ginez <175813+eginez@users.noreply.github.com>
2020-10-12 12:13:46 +0200

    Update clustersparkgen.sh

--- ml/clustersparkgen.sh
+++ ml/clustersparkgen.sh
@@ -11,7 +11,7 @@ scp lenet5.py  $1:/tmp/ml_scripts
 scp $4  $1:/tmp/ml_scripts
 ## TODO add more setup steps
 
-ssh -o StrictHostKeyChecking=no $1 ./home/am72ghiassi/bd/sparkgen -bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json
+ssh -o StrictHostKeyChecking=no $1 ./home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json
 mkdir -p results
 scp $1:/tmp/ml_scripts/lenet5.py/result.csv results/$3.csv
 gsutil cp results/$3.csv gs://qpecs-output
