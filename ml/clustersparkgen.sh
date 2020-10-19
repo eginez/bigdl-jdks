42fde475552bee078b081c0783eae88ed4e92074
shreyan2020 <60085842+shreyan2020@users.noreply.github.com>
2020-10-19 18:37:01 +0200

    Update clustersparkgen.sh
    adding queueing theory logic

--- ml/clustersparkgen.sh
+++ ml/clustersparkgen.sh
@@ -8,12 +8,13 @@
 ssh -o StrictHostKeyChecking=no $1 mkdir -p /tmp/ml_scripts
 scp lenet5.py  $1:/tmp/ml_scripts
 ## Copy generated conf.json
-scp $4  $1:/tmp/ml_scripts
+scp /home/shrey/bigdl-jdks/infra/conf.json  $1:/tmp/ml_scripts
 ## TODO add more setup steps
 
-ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json
 mkdir -p results
-scp $1:/home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/result.csv results/$3.csv
+ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json > results/measurements.txt
+python3 parse_log.py -l results/measurements.txt -m results/queue-$3.csv
+
 #when running more than two workloads
 #scp $1:/home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/result1.csv results1/$3.csv
-gsutil cp results/$3.csv gs://qpecs-output
+gsutil cp results/queue-$3.csv gs://qpecs-output
