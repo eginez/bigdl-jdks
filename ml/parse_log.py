6f366902190039bebb232a3f68cd072d85256923
Wessel van de Brug <wvdbrug@gmail.com>
2020-10-19 14:37:09 +0200

    Adjust log parser to parse SparkGen result file instead of standard output

--- ml/parse_log.py
+++ ml/parse_log.py
@@ -14,22 +14,17 @@ def main():
     arguments = argument_parser.parse_args()
 
     # Retrieve measurements from log
-    inter_arrival_times = []
-    service_times = []
+    rows = []
     with open(arguments.log) as log_file:
         content = "".join(log_file.readlines())
-        for match in re.finditer(".*Waiting ([\\d.]+) seconds for new job.*", content):
-            inter_arrival_time = float(match.group(1))
-            inter_arrival_times.append(inter_arrival_time)
-        for match in re.finditer(".*ended with status.*,[\\d.]+,([\\d.]+),([\\d.]+)\n.*", content):
-            start_time = float(match.group(1))
-            end_time = float(match.group(2))
-            service_time = end_time - start_time
-            service_times.append(service_time)
+        for match in re.finditer(".*,([\\d.])+,([\\d.]+),([\\d.]+).*", content):
+            arrival_time = float(match.group(1))
+            start_time = float(match.group(2))
+            end_time = float(match.group(3))
+            rows.append([arrival_time, start_time, end_time])
 
     # Create and store dataframe
-    data = list(zip(inter_arrival_times, service_times))
-    data_frame = pd.DataFrame(data, columns=["inter-arrival time", "service time"])
+    data_frame = pd.DataFrame(rows, columns=["arrival time", "start time", "end time"])
     data_frame.to_csv(arguments.measurements)
 
 
