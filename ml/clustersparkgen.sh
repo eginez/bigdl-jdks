a8f2d271d36a131c67f9b45ca58cc012112078f6
shreyan2020 <60085842+shreyan2020@users.noreply.github.com>
2020-10-21 21:54:51 +0200

    Update clustersparkgen.sh
    some small fixes

--- ml/clustersparkgen.sh
+++ ml/clustersparkgen.sh
@@ -6,17 +6,17 @@
 
 
 ssh -o StrictHostKeyChecking=no $1 mkdir -p /tmp/ml_scripts
-scp lenet5.py  $1:/tmp/ml_scripts
+scp ../ml/bi-rnn.py  $1:/tmp/ml_scripts
 ## Copy generated conf.json
 scp /home/shrey/bigdl-jdks/infra/conf.json  $1:/tmp/ml_scripts
 ## TODO add more setup steps
 
 mkdir -p results
-ssh -o StrictHostKeyChecking=no $1 cat /tmp/ml_scripts/conf.json
-ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json > results/measurements.txt
-python3 parse_log.py -l results/measurements.txt -m results/$3.csv
+#ssh -o StrictHostKeyChecking=no $1 cat /tmp/ml_scripts/conf.json
+#ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/sparkgen -r -d -c /tmp/ml_scripts/conf.json > results/measurements.txt
+#python3 parse_log.py -l results/measurements.txt -m results/$3.csv
 
-scp -R $1:/home/shrey/test /python_results/
+#scp -R $1:/home/shrey/test /python_results/
 #when running more than two workloads
 #scp $1:/home/am72ghiassi/bd/sparkgen-bigdl/src/sparkgen/result1.csv results1/$3.csv
-gsutil cp results/$3.csv gs://qpecs-output/queue
+#gsutil cp results/$3.csv gs://qpecs-output/queue
