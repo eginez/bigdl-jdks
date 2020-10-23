36fd87a20fc100d3e25ed53ba3ebb6dfe5f4fcae
shreyan2020 <60085842+shreyan2020@users.noreply.github.com>
2020-10-23 12:00:18 +0200

    Update clusterlenet.sh
    handling utils file movement

--- ml/clusterlenet.sh
+++ ml/clusterlenet.sh
@@ -8,6 +8,7 @@ ssh-keygen -R $1
 ##Creating folder in master node to store workload
 ssh -o StrictHostKeyChecking=no $1 mkdir -p /tmp/ml_scripts
 scp lenet5.py  $1:/tmp/ml_scripts
+scp utils.py  $1:/tmp/ml_scripts
 ## Executing the workload
 ssh -o StrictHostKeyChecking=no $1 /home/am72ghiassi/bd/spark/bin/spark-submit --master spark://$1:7077 --driver-cores 1 --driver-memory 1G --total-executor-cores 2 --executor-cores 1 --executor-memory 1G --py-files /home/am72ghiassi/bd/spark/lib/bigdl-0.11.0-python-api.zip,/tmp/ml_scripts/lenet5.py --properties-file /home/am72ghiassi/bd/spark/conf/spark-bigdl.conf --jars /home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.driver.extraClassPath=/home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.executer.extraClassPath=bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar  /tmp/ml_scripts/lenet5.py --action train --dataPath /tmp/mnist -b $2
 ##Processing results and storing them in google cloud storage bucket
